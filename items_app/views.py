a8de0daabf0733faeeb4041ddbfbf69b0e005bf7
YeTigabLigKindsew <tigabKindsew@gmail.com>
2026-02-03 00:42:30 -0800

    Add superuser restriction to item_form view
    Restrict access to item_form view to superusers only.

--- items_app/views.py
+++ items_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
 from .forms import ItemForm
 
+
+@user_passes_test(lambda u: u.is_superuser)
 def item_form(request):
   if request.method == "POST":
     form = ItemForm(request.POST, request.FILES)
